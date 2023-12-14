770239373e4a74bd7d1bab643c9acab63f1d7525
xujian <908634674@qq.com>
2023-12-14 18:20:54 +0800

    ios

--- rnTemplate/ios/duxapp/AppDelegate.h
+++ rnTemplate/ios/duxapp/AppDelegate.h
@@ -4,22 +4,12 @@
 
 // {#duxapp-insert import}
 
-// react-native-wechat-lib start
-#import "WXApi.h"
-// react-native-wechat-lib end
-
-// react-native-dux-push start
-#import <UserNotifications/UserNotifications.h>
-// react-native-dux-push end
+@protocol EmptyDelegate
+@end
 
 @interface AppDelegate : EXAppDelegateWrapper <
-  // {#duxapp-insert appDelegate}
-  // react-native-wechat-lib start
-  WXApiDelegate
-  // react-native-wechat-lib end
-  // react-native-dux-push start
-  ,UNUserNotificationCenterDelegate
-  // react-native-dux-push end
+  EmptyDelegate
+  // {#duxapp-insert appDelegate.protocol}
 >
-
+  // {#duxapp-insert appDelegate}
 @end
--- rnTemplate/ios/duxapp/AppDelegate.mm
+++ rnTemplate/ios/duxapp/AppDelegate.mm
@@ -6,25 +6,10 @@
 
 // {#duxapp-insert import}
 
-// react-native-bootsplash start
-#import "RNBootSplash.h"
-// react-native-bootsplash end
-
-#import <CodePush/CodePush.h>
-
-// react-native-dux-push start
-#import <RNDuxPush.h>
-// react-native-dux-push end
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-
-  // react-native-dux-push start
-  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-  [RNDuxPush application:application didFinishLaunchingWithOptions:launchOptions];
-  // react-native-dux-push end
 
   // {#duxapp-insert appDelegate.didFinishLaunchingWithOptions}
 
@@ -41,9 +26,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [CodePush bundleURL];
-  // {#duxapp appDelegate.sourceURLForBridge 'return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];'}
-//  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  {#duxapp appDelegate.sourceURLForBridge 'return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];'}
 #endif
 }
 
@@ -58,112 +41,4 @@
 }
 
 // {#duxapp-insert appDelegate}
-
-// react-native-bootsplash start
-- (UIView *)createRootViewWithBridge:(RCTBridge *)bridge
-                          moduleName:(NSString *)moduleName
-                           initProps:(NSDictionary *)initProps {
-  UIView *rootView = [super createRootViewWithBridge:bridge
-                                          moduleName:moduleName
-                                           initProps:initProps];
-
-  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView];
-
-  return rootView;
-}
-// react-native-bootsplash end
-
-// react-native-dux-push start
-- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
-{
-
-  [RNDuxPush application:application didRegisterUserNotificationSettings:notificationSettings];
-
-}
-
-- (void)application:(UIApplication *)app
-didFailToRegisterForRemoteNotificationsWithError:(NSError *)err
-{
-  NSLog(@"推送注册失败：%@", err);
-}
-
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
-  [RNDuxPush application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
-
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification
-{
-  [RNDuxPush application:application didReceiveRemoteNotification:notification];
-
-}
-
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
-  [RNDuxPush application:application didReceiveLocalNotification:notification];
-
-}
-
-// ios 10
-// 应用在前台收到通知
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
-  [RNDuxPush userNotificationCenter:center willPresentNotification:notification withCompletionHandler:completionHandler];
-}
-
-// 点击通知进入应用
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
-  [RNDuxPush userNotificationCenter:center didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
-  completionHandler();
-}
-
-// react-native-dux-push end
-
-
-// react-native-wechat-lib start
-
-- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
-    return  [WXApi handleOpenURL:url delegate:self];
-}
-
-- (BOOL)application:(UIApplication *)application
-  continueUserActivity:(NSUserActivity *)userActivity
-  restorationHandler:(void(^)(NSArray<id<UIUserActivityRestoring>> * __nullable
-  restorableObjects))restorationHandler {
-  // 触发回调方法
-  [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
-  return [WXApi handleOpenUniversalLink:userActivity
-  delegate:self];
-}
-
-// Universal Links 配置文件, 没使用的话可以忽略。
-// ios 9.0+
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
-            options:(NSDictionary<NSString*, id> *)options
-{
-  // Triggers a callback event.
-  // 触发回调事件
-  [RCTLinkingManager application:application openURL:url options:options];
-  return [WXApi handleOpenURL:url delegate:self];
-}
-// react-native-wechat-lib end
-
-// react-native-alipay start
-
-- (BOOL)application:(UIApplication *)application
-            openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication
-         annotation:(id)annotation
-{
-  return [RCTLinkingManager application:application openURL:url
-                      sourceApplication:sourceApplication annotation:annotation];
-}
-
-//- (BOOL)application:(UIApplication *)application
-//            openURL:(NSURL *)url
-//            options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options
-//{
-//  return [RCTLinkingManager application:application openURL:url options:options];
-//}
-
-// react-native-alipay end
 @end
