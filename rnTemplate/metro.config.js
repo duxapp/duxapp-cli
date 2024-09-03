3dd1643cd7fc031dcbd862cba5c50f7400221da1
shaogong <908634674@qq.com>
2024-09-03 21:27:36 +0800

    更新rn配置

--- rnTemplate/metro.config.js
+++ rnTemplate/metro.config.js
@@ -1,20 +1,11 @@
 /* eslint-disable import/no-commonjs */
-const { mergeConfig } = require("metro-config");
-const { getMetroConfig } = require("@tarojs/rn-supporter");
+const { mergeConfig } = require('metro-config')
+const { getMetroConfig } = require('@tarojs/rn-supporter')
 
-module.exports = mergeConfig(
-  {
-    // custom your metro config here
-    // https://facebook.github.io/metro/docs/configuration
-    resolver: {},
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-    },
-  },
-  getMetroConfig(),
-);
+module.exports = (async function (){
+  return mergeConfig({
+  // custom your metro config here
+  // https://facebook.github.io/metro/docs/configuration
+    resolver: {}
+  }, await getMetroConfig())
+})()
