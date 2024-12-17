f8c9973e8df1e925a19bd5a1647a3bda47a413c3
duxapp <908634674@qq.com>
2024-12-17 20:54:41 +0800

    2.1.4

--- appTemplate/pages/index/index.jsx
+++ appTemplate/pages/index/index.jsx
@@ -5,8 +5,8 @@ export default TopView.HOC(function IndexPage() {
   return <>
     <Header title='{{name}}' titleCenter />
     <ScrollView>
-      <View className='gap-3'>
-        <Text className='text-c1 text-s5 bold'>欢迎使用duxapp</Text>
+      <View className='gap-3 p-3'>
+        <Text className='text-c1 text-s7 bold'>欢迎使用duxapp</Text>
         <Text className='text-c1 text-s3'>新创建的模块默认依赖于duxapp模块</Text>
         <Text className='text-c1 text-s3'>
           如果需要依赖于其他的模块请在app.json中修改依赖项, 并且使用 yarn duxapp
